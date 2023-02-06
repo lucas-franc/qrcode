23a0b5652bffd17e5eee623565a1f8794b07cd6a
Lucas França <lucas.fnev@gmail.com>
2023-02-06 17:51:27 -0300

    accessing native resources (camera)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qrcode/src/camera.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: const Camera(),
     );
   }
 }
