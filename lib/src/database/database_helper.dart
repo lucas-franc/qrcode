6be93d20321de87563bc726fd77de04aa0883e06
Lucas França <lucas.fnev@gmail.com>
2023-02-13 12:41:22 -0300

    creating database and database methods, creating form

---
+++ lib/src/database/database_helper.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static const _databaseName = "MembersDB";
+  static const _databaseVersion = 1;
+  static const table = 'member';
+
+  static const columnId = '_id';
+  static const columnName = 'name';
+  static const columnOcupation = 'ocupation';
+  static const columnCpf = 'cpf';
+  static const columnStatus = 'status';
+  static const columnPhoto = 'photo';
+
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database _database = DatabaseHelper._database;
+
+  Future<Database> get database async {
+    _database = await _initDatabase();
+    return _database;
+  }
+
+  _initDatabase() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, _databaseName);
+    return await openDatabase(path,
+        version: _databaseVersion, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute("""
+CREATE TABLE $table (
+  $columnId INTEGER PRIMARY KEY AUTOINCREMENT
+  $columnName TEXT NOT NULL,
+  $columnCpf INTEGER NOT NULL,
+  $columnOcupation TEXT NOT NULL,
+  $columnPhoto TEXT NOT NULL,
+  $columnStatus TEXT NOT NULL,
+)
+""");
+  }
+
+  Future<int> insert(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    return await db.insert(table, row);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAllRows() async {
+    Database db = await instance.database;
+    return await db.query(table);
+  }
+
+  Future<int?> queryRowCount() async {
+    Database db = await instance.database;
+    return Sqflite.firstIntValue(
+        await db.rawQuery('SELECT COUNT(*) FROM $table'));
+  }
+
+  Future<int> update(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    int id = row[columnId];
+    return await db.update(table, row, where: '$columnId = ?', whereArgs: [id]);
+  }
+
+  Future<int> delete(int id) async {
+    Database db = await instance.database;
+    return await db.delete(table, where: '$columnId = ?', whereArgs: [id]);
+  }
+}
--- lib/src/home/home_view.dart
+++ lib/src/home/home_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:qrcode/src/camera/camera.dart';
-import 'package:qrcode/src/scan/scan.dart';
+import 'package:qrcode/src/database/database_helper.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -10,6 +9,9 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final dbHelper = DatabaseHelper.instance;
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController functionController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,21 +20,60 @@ class _HomeViewState extends State<HomeView> {
           padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
           child: Column(
             children: [
-              const Camera(),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const QrCodeScannerView(),
-                    ),
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  label: Text("Nome"),
+                ),
+                onSubmitted: (value) {
+                  setState(
+                    () {
+                      functionController.text.toString();
+                    },
                   );
                 },
-                child: const Text('SCAN'),
               ),
+              TextField(
+                controller: functionController,
+                decoration: const InputDecoration(
+                  label: Text("Função"),
+                ),
+                onSubmitted: (value) {
+                  setState(
+                    () {
+                      functionController.text.toString();
+                    },
+                  );
+                },
+              ),
+              Text(
+                nameController.text.toString(),
+              ),
+              Text(
+                functionController.text.toString(),
+              ),
+              //Camera(),
+              // ElevatedButton(
+              //   onPressed: () {
+              //     Navigator.of(context).push(
+              //       MaterialPageRoute(
+              //         builder: (context) => const QrCodeScannerView(),
+              //       ),
+              //     );
+              //   },
+              //   child: const Text('SCAN'),
+              // ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  void _insert() async {
+    // linha para incluir
+    Map<String, dynamic> row = {};
+    final id = await dbHelper.insert(row);
+    print('linha inserida id: $id');
   }
 }
